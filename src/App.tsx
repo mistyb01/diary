7e88cff88c810b59dfe94248fc48f8d31ab2f275
Misty Becker <mistyb01@uw.edu>
2023-10-27 15:18:32 -0700

    setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import JournalContainer from "./components/JournalContainer";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <>
-      <JournalContainer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/journal" Component={JournalContainer} />
+        <Route path="/login" Component={Login} />
+      </Routes>
+    </Router>
   );
 }
 
