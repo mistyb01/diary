a430f4396ea7e7b7f2ccfe56f98281e060151b0e
Misty Becker <mistyb01@uw.edu>
2023-10-27 19:20:02 -0700

    add toggle to side panel

--- src/components/JournalContainer/JournalContainer.tsx
+++ src/components/JournalContainer/JournalContainer.tsx
@@ -1,5 +1,6 @@
 import SidePanel from "./SidePanel";
 import EntryContainer from "./EntryContainer";
+import { useState } from "react";
 
 const JournalContainer = () => {
   return (
--- src/components/JournalContainer/SidePanel/SidePanel.tsx
+++ src/components/JournalContainer/SidePanel/SidePanel.tsx
@@ -1,10 +1,27 @@
 import YearListItem from "./YearListItem";
+import { useState } from "react";
+import { FiChevronsLeft } from "react-icons/fi";
+import { FiChevronsRight } from "react-icons/fi";
 
 const SidePanel = () => {
+  const [showPanel, setShowPanel] = useState(true);
+
   return (
     <section className="side-panel-container">
-      <h1 className="accent-label">entry list</h1>
-      <YearListItem />
+      <div
+        className="side-panel-toggle-container"
+        onClick={() => setShowPanel(!showPanel)}
+      >
+        {showPanel ? <FiChevronsLeft /> : <FiChevronsRight />}
+      </div>
+      {showPanel && (
+        <>
+          <h1 className="accent-label">entry list</h1>
+
+          {/* TODO: items are mapped to entry data */}
+          {showPanel && <YearListItem />}
+        </>
+      )}
     </section>
   );
 };
