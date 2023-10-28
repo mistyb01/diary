a80bfdcb7e9b66df60e805c72a17ff88f82d9a86
Misty Becker <mistyb01@uw.edu>
2023-10-27 17:38:00 -0700

    create mock data of entries

--- src/components/JournalContainer/SidePanel/YearListItem/MonthListItem/MonthListItem.tsx
+++ src/components/JournalContainer/SidePanel/YearListItem/MonthListItem/MonthListItem.tsx
@@ -1,7 +1,7 @@
-const MonthListItem = () => {
+const MonthListItem = ({ month }: { month: number }) => {
   return (
     <>
-      <h3>month</h3>
+      <h3>{month}</h3>
     </>
   );
 };
--- src/components/JournalContainer/SidePanel/YearListItem/YearListItem.tsx
+++ src/components/JournalContainer/SidePanel/YearListItem/YearListItem.tsx
@@ -1,10 +1,77 @@
 import MonthListItem from "./MonthListItem/MonthListItem";
 
 const YearListItem = () => {
+  const mockData = [
+    {
+      userID: 1,
+      title: "First Entry",
+      text_body: "This is my first diary entry. I had a great day today.",
+      creation_timestamp: "2023-10-27T09:00:00",
+    },
+    {
+      userID: 2,
+      title: "Memorable Hike",
+      text_body:
+        "I went on a memorable hike today. The scenery was breathtaking.",
+      creation_timestamp: "2023-10-26T17:30:00",
+    },
+    {
+      userID: 3,
+      title: "Family Dinner",
+      text_body: "Had a wonderful family dinner at my favorite restaurant.",
+      creation_timestamp: "2023-10-25T20:15:00",
+    },
+    {
+      userID: 4,
+      title: "Productive Workday",
+      text_body:
+        "Today was a very productive day at work. I accomplished a lot.",
+      creation_timestamp: "2023-10-24T16:45:00",
+    },
+    {
+      userID: 5,
+      title: "Movie Night",
+      text_body: "Spent the evening watching a great movie with friends.",
+      creation_timestamp: "2023-10-23T21:00:00",
+    },
+    {
+      userID: 1,
+      title: "Weekend Getaway",
+      text_body: "Had a relaxing weekend getaway by the beach.",
+      creation_timestamp: "2023-10-22T14:20:00",
+    },
+    {
+      userID: 2,
+      title: "Gardening Day",
+      text_body: "Spent the day gardening and planting new flowers.",
+      creation_timestamp: "2023-10-21T10:30:00",
+    },
+    {
+      userID: 3,
+      title: "Birthday Celebration",
+      text_body:
+        "Celebrated my birthday with friends and family. It was a blast!",
+      creation_timestamp: "2023-10-20T18:00:00",
+    },
+    {
+      userID: 4,
+      title: "Rainy Day Reading",
+      text_body: "Enjoyed a cozy day indoors, reading my favorite book.",
+      creation_timestamp: "2023-10-19T12:45:00",
+    },
+    {
+      userID: 5,
+      title: "New Recipe Experiment",
+      text_body:
+        "Tried a new recipe for dinner tonight. It turned out delicious!",
+      creation_timestamp: "2023-10-18T19:30:00",
+    },
+  ];
+
   return (
     <>
       <h2>2023</h2>
-      <MonthListItem />
+      <MonthListItem month={0} />
     </>
   );
 };
