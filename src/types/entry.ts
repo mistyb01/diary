e70588f0c190c7ae7943bb5aca0540c7b54800f2
Misty Becker <mistyb01@uw.edu>
2023-10-30 16:36:21 -0700

    create entry interface

---
+++ src/types/entry.ts
@@ -0,0 +1,10 @@
+interface entry {
+    id: number,
+    userID: number,
+    title: string,
+    text_body: string,
+    creation_timestamp: string, // should later be date
+}
+
+
+export default entry;
